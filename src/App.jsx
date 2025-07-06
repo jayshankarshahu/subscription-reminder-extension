04b6b8ac8e478f0a58f11c8565364a8ef3234958
Jayshankar <jayshankarshahu@gmail.com>
2025-07-06 11:21:55 +0530

    Created timer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      <TimerInput/>
+      <TimerInput onchange={() => {
+        console.log("Input changed");
+      }}/>
     </>
   )
 
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,42 +1,43 @@
-import { useState } from "react";
-
-function TimerInput({ days = 0, hours = 0, minutes = 0, seconds = 0 }) {
-
-    let daysPadded = String(days).padStart(2, '0');
-    let hoursPadded = String(hours).padStart(2, '0');
-    let minutesPadded = String(minutes).padStart(2, '0');
-    let secondsPadded = String(seconds).padStart(2, '0');
+function TimerInput({ days = 0, hours = 0, minutes = 0, seconds = 0 , onchange = () => {}}) {
 
     return (
 
         <div className="flex items-center justify-center w-full gap-1.5 count-down-main">
             <div className="timer">
-                <div className="rounded-xl border border-gray-50 py-1.5 flex items-center justify-center flex-col gap-0 aspect-square px-1.5">
-                    <input type="text" pattern="\d*" inputMode="numberic" className="bg-gray-50 border max-w-[50px] text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="DD" required defaultValue={daysPadded} />
-                    <p className="text-sm font-inter capitalize font-normal text-gray-600 text-center w-full">days</p>
-                </div>
+                <NumberInput defaultValue={days} label="Days" placeholder="00" onchange={onchange}/>
             </div>
             <h3 className="font-manrope font-semibold text-2xl text-gray-900">:</h3>
             <div className="timer">
-                <div className="rounded-xl border border-gray-50 py-1.5 flex items-center justify-center flex-col gap-0 aspect-square px-1.5">
-                    <input type="text" pattern="\d*" inputMode="numberic" className="bg-gray-50 border max-w-[50px] text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="HH" required defaultValue={hoursPadded} />
-                    <p className="text-sm font-inter capitalize font-normal text-gray-600 text-center w-full">Hour</p>
-                </div>
+                <NumberInput defaultValue={hours} label="Hours" placeholder="00" maxValue="23" onchange={onchange}/>
             </div>
             <h3 className="font-manrope font-semibold text-2xl text-gray-900">:</h3>
             <div className="timer">
-                <div className="rounded-xl border border-gray-50 py-1.5 flex items-center justify-center flex-col gap-0 aspect-square px-1.5">
-                    <input type="text" pattern="\d*" inputMode="numberic" className="bg-gray-50 border max-w-[50px] text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM" required defaultValue={minutesPadded} />
-                    <p className="text-sm font-inter capitalize font-normal text-gray-600 text-center w-full">Minutes</p>
-                </div>
+                <NumberInput defaultValue={minutes} label="Minutes" placeholder="00" maxValue="59" onchange={onchange}/>
             </div>
             <h3 className="font-manrope font-semibold text-2xl text-gray-900">:</h3>
             <div className="timer">
-                <div className="rounded-xl border border-gray-50 py-1.5 flex items-center justify-center flex-col gap-0 aspect-square px-1.5">
-                    <input type="text" pattern="[0-9]+" inputMode="numberic" className="bg-gray-50 border max-w-[50px] text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="SS" required defaultValue={secondsPadded} />
-                    <p className="text-sm font-inter capitalize font-normal text-gray-600 text-center w-full">Seconds</p>
-                </div>
+                <NumberInput defaultValue={seconds} label="Seconds" placeholder="00" maxValue="59" oonchange={onchange}nchange/>
             </div>
+        </div>
+
+    );
+
+}
+
+function NumberInput({ maxValue, defaultValue = 0, paddedLength = 2, label = "", placeholder = "" , onchange = () => {}}) {
+
+    let valuePadded = String(defaultValue).padStart(paddedLength, '0');
+
+    return (
+        <div className="rounded-xl border border-gray-50 py-1.5 flex items-center justify-center flex-col gap-0 aspect-square px-1.5">
+            <input type="number" inputMode="numberic" min={0} className="hide-arrow bg-gray-50 border max-w-[50px] text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder={placeholder}
+                required
+                defaultValue={valuePadded}
+                max={maxValue || 9999}
+                onChange={onchange}
+            />
+            <p className="text-sm font-inter capitalize font-normal text-gray-600 text-center w-full">{label}</p>
         </div>
     );
 
